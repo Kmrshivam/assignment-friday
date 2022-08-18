9d15252b666767e9fcebb5c9e9b8117e2ad5eb7e
Shivam Kumar <shivamkmr2025@gmail.com>
2022-08-18 23:30:40 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [res, setRes] = useState('')
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const dataToSubmit = {
+      name,
+      email,
+      lastName,
+    }
+    fetch('https://reqres.in/api/users?page=2/posts', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(dataToSubmit)
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res)
+        alert('FORM submit successful')
+      })
+  }
+  useEffect(() => {
+    fetch('https://reqres.in/api/users?page=2', {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setRes(res.data)
+      })
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form method='post' action="#" onSubmit={handleFormSubmit}>
+        <lable>Name:
+          <input type="text" value={name} onChange={(event) => setName(event.target.value)} name="name" />
+        </lable><br /><br />
+        <lable>LastName:
+          <input type="text" value={lastName} onChange={(event) => setLastName(event.target.value)} name="lastname" />
+        </lable><br /><br />
+        <lable>Email:
+          <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} name="email" />
+        </lable><br /><br />
+        <p>{JSON.stringify(res)}</p>
+        <input type="submit" />
+        <input type="reset" />
+      </form>
     </div>
   );
 }
